a9cd0375bae52c27330aa3df8a5bdcd9e9089c9c
Maciej Kleban <dawnwender@gmail.com>
2022-01-04 23:34:26 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -40,3 +40,73 @@ struct Credentials getCredentials(char input[]) {
     strcpy(result.password, password);
     return result;
 }
+
+
+// Extract username from input
+void getUser(char input[], char *output) {
+    strcpy(output, "");
+    int i = 14;
+    while(input[i] != '"') {
+        output[i-14] = input[i];
+        i++;
+    }
+    output[i-14] = '\0';
+}
+
+
+// Extract message, sender and receiver from input
+struct Message getMessage(char input[]) {
+    struct Message msg;
+    char tmp[4096] = "";
+
+    // Skip {"
+    int pos = 2;
+    int i = 0;
+    // Skip sender key
+    while(input[pos] != '"') pos++;
+    // Skip ":"
+    pos = pos + 4;
+    // Read sender
+    while(input[pos] != '"') {
+        tmp[i] = input[pos];
+        pos++;
+        i++;
+    }
+    tmp[i] = '\0';
+    i = 0;
+    // Skip ","
+    pos = pos + 4;
+    strcpy(msg.sender, tmp);
+    strcpy(tmp, "");
+
+    // Skip receiver key
+    while(input[pos] != '"') pos++;
+    // Skip ":"
+    pos = pos + 4;
+    // Read receiver
+    while(input[pos] != '"') {
+        tmp[i] = input[pos];
+        pos++;
+        i++;
+    }
+    tmp[i] = '\0';
+    i = 0;
+    // Skip ","
+    pos = pos + 4;
+    strcpy(msg.receiver, tmp);
+    strcpy(tmp, "");
+
+    // Skip content key
+    while(input[pos] != '"') pos++;
+    // Skip ":"
+    pos = pos + 4;
+    // Read content
+    while(input[pos] != '"') {
+        tmp[i] = input[pos];
+        pos++;
+        i++;
+    }
+    tmp[i] = '\0';
+    strcpy(msg.content, tmp);
+    return msg;
+}

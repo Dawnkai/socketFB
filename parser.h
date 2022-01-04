016a67c30a1e4de9ba71a9b4e37bf71f7363e3a3
Maciej Kleban <dawnwender@gmail.com>
2022-01-04 23:34:37 +0100

    Update parser.h

--- parser.h
+++ parser.h
@@ -11,7 +11,17 @@ struct Credentials {
     char password[4096];
 };
 
+struct Message {
+    char sender[4096];
+    char receiver[4096];
+    char content[4096];
+};
+
 // Extract credentials from input string
 struct Credentials getCredentials(char input[]);
+// Extract username from input string
+void getUser(char input[], char *output);
+// Extract components of message string
+struct Message getMessage(char input[]);
 
 #endif
